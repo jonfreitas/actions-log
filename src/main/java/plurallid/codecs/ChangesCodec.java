4bebd9d11b2de75d6c51fad84fb908997451a820
Jonathan Freitas <jonathanvinicius91@gmail.com>
2023-08-17 14:58:59 +0000

    [PID-5375] - Discard 'Old Data' and 'New Data' fields from project

--- src/main/java/plurallid/codecs/ChangesCodec.java
+++ src/main/java/plurallid/codecs/ChangesCodec.java
@@ -7,55 +7,16 @@ import jakarta.enterprise.context.ApplicationScoped;
 import org.bson.Document;
 import org.bson.conversions.Bson;
 import plurallid.entity.actions.*;
-import plurallid.repository.actions.ActionRepository;
-
-import java.time.LocalDateTime;
 import java.util.*;
 
 import static com.mongodb.client.model.Aggregates.project;
 import static com.mongodb.client.model.Filters.eq;
 import static com.mongodb.client.model.Projections.*;
-import static com.mongodb.client.model.Projections.computed;
 
 
 @ApplicationScoped
 public class ChangesCodec {
 
-    private final ActionRepository actionRepository = new ActionRepository();
-
-    public List<Changes> listChanges() {
-        List<Changes> list = new ArrayList<>();
-
-        try (MongoCursor<Document> cursor = actionRepository.getCollection().find().iterator()) {
-            while (cursor.hasNext()) {
-                Document document = cursor.next();
-                Changes changes = new Changes();
-                changes.setOldData(document.getString("oldData"));
-                changes.setNewData(document.getString("newData"));
-                changes.setApplicationId(document.getString("message"));
-                changes.setWhen(LocalDateTime.parse(document.getString("when")));
-                changes.setMessage(document.getString("applicationId"));
-                list.add(changes);
-            }
-        }
-        return list;
-    }
-
-    public List<WhoChange> listWho() {
-        List<WhoChange> list = new ArrayList<>();
-
-        try (MongoCursor<Document> cursor = actionRepository.getCollection().find().iterator()) {
-            while (cursor.hasNext()) {
-                Document document = cursor.next();
-                WhoChange who = new WhoChange();
-                who.setPersonId(document.getInteger("personId"));
-                who.setAdmId(document.getInteger("admId"));
-                list.add(who);
-            }
-        }
-        return list;
-    }
-    
     public void addNewChanges(Action action, MongoCollection<Document> collection) {
         int count = this.getChangesTotalCount(action.getEntityValue(), action.getEntityId(), collection);
         this.recordChangesData(action, collection);
@@ -64,21 +25,19 @@ public class ChangesCodec {
 
     private void recordChangesData(Action action, MongoCollection<Document> collection) {
         Document documentChanges = new Document()
-                .append("oldData", action.getChanges().get(0).getOldData())
-                .append("newData", action.getChanges().get(0).getNewData())
+                .append("applicationId", action.getChanges().get(0).getApplicationId())
                 .append("message", action.getChanges().get(0).getMessage())
-                .append("when", new Date())
-                .append("applicationId", action.getChanges().get(0).getApplicationId());
+                .append("when", new Date());
         collection.updateOne(eq("entityValue", action.getEntityValue()),
                 Updates.addToSet("changes", documentChanges));
     }
 
     private void recordWhoData(Action action, MongoCollection<Document> collection, Integer count) {
         Document documentWho = new Document()
-                .append("personId", action.getChanges().get(0).getWho().get(0).getPersonId())
-                .append("admId", action.getChanges().get(0).getWho().get(0).getAdmId());
-        collection.updateMany(eq("entityValue", action.getEntityValue()),
-                Updates.addToSet("changes." + count + ".who", documentWho));
+                .append("personId", action.getChanges().get(0).getWho().getPersonId())
+                .append("admId", action.getChanges().get(0).getWho().getAdmId());
+        collection.updateMany(eq("entityValue", action.getEntityValue()),  
+            Updates.set("changes." + count + ".who", documentWho));
     }
 
     public int getChangesTotalCount(Integer entityValue, Integer entityId, MongoCollection<Document> collection) {
